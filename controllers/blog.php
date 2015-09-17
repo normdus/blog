e853d1db5016e848171df52538bd8812bc4a636c
nduchene <norman.duchene@comcast.net>
2015-09-17 11:42:19 -0400

    comment test good

--- controllers/blog.php
+++ controllers/blog.php
@@ -9,13 +9,12 @@
 
 include_once "models/Blog_Entry_Table.class.php";
 $entryTable = new Blog_Entry_Table( $db );
-$entries = $entryTable->getAllEntries();
 
 $isEntryClicked = isset( $_GET['id'] );
 if ( $isEntryClicked) {
 	//show one entry with specific ID
-	$entryID = $_GET['id'];
-	$entryData = $entryTable->getEntry( $entryID );
+	$entryId = $_GET['id'];
+	$entryData = $entryTable->getEntry( $entryId );
 	$blogOutput = include_once "views/entry-html.php";
 	
 //  Complex View - A view made up of multiple controllers, views, models.	
